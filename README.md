b6f1f81b235103e9286864f9688079bcbdbd9dc3
tlmohren <tlmohren@uw.edu>
2020-04-07 20:23:17 -0700

    added logging at 0s and stop

---
+++ README.md
@@ -0,0 +1,15 @@
+# Work timer
+A GUI experiment using PyQt5.
+
+## Goals:
+- have a timer in lower right corner that logs tasks
+- have it be a stable executable file
+- use non-intrusive graphics (e.g. transparency? )
+
+
+## Ideas to look at:
+https://stackoverflow.com/questions/33982167/pyqt5-create-semi-transparent-window-with-non-transparent-children
+
+## issues:
+- when time runs out, button is still named pause, switching function not accessible from that branch
+- graphics still too intrusive, not simple enough 
--- Timer_frame.py
+++ Timer_frame.py
@@ -27,36 +27,36 @@ import functools
 
 import datetime 
 import time
+ 
 
-# Create a Controller class to connect the GUI and the model
-class PyCalcCtrl:
-    """PyCalc Controller class."""
-    def __init__(self, view):
-        """Controller initializer."""
-        self._view = view
-        # Connect signals and slots
-        self._connectSignals()
+import csv
 
-    def _buildExpression(self, sub_exp):
-        """Build expression."""
-        expression = self._view.displayText() + sub_exp
-        self._view.setDisplayText(expression)
 
-    def _connectSignals(self):
-        """Connect signals and slots."""
-        for btnText, btn in self._view.buttons.items():
-            if btnText not in {'=', 'C'}:
-                btn.clicked.connect(partial(self._buildExpression, btnText))
-
-        self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+  
+ 
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = csv.writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
 
 
 # Create a subclass of QMainWindow to setup the calculator's GUI
 class PyPomodoro(QMainWindow): 
 
-
+    QMainWindow.n_sec = 10
     QMainWindow.pause = True
-    QMainWindow.countDown = 25*60 
+    QMainWindow.countDown = QMainWindow.n_sec
+
+    QMainWindow.task = 'code'
+    QMainWindow.label = 'study'
+    QMainWindow.date = QDate.currentDate().toString("dd/MM/yyyy") 
+    QMainWindow.t0 = QTime.currentTime().toString('hh:mm:ss') 
+
+
+
 
     def _createDisplay(self):
         """Create the display."""
@@ -77,12 +77,30 @@ class PyPomodoro(QMainWindow):
         # currentTime = QTime.currentTime()
         # displayTxt = currentTime.toString('hh:mm:ss')  
  
-        if QMainWindow.pause == False: 
+        if (QMainWindow.pause == False) & (QMainWindow.countDown>0): 
             QMainWindow.countDown = QMainWindow.countDown - 1
             displayTxt = time.strftime('%M:%S', time.gmtime(QMainWindow.countDown))
         #     print(self.pause) 
-        else: 
+        elif (QMainWindow.pause == True): 
             displayTxt = time.strftime('%M:%S', time.gmtime(QMainWindow.countDown))
+        else:
+            print('reset time')
+            QMainWindow.pause = True 
+            displayTxt = time.strftime('%M:%S', time.gmtime(QMainWindow.countDown)) 
+            print('log countdown')
+
+
+
+            output_list= [QMainWindow.date, 
+                QMainWindow.t0, 
+                QMainWindow.task, 
+                QMainWindow.label, 
+                str(QMainWindow.n_sec - QMainWindow.countDown)]  
+            append_list_as_row('task_log.csv', output_list) 
+            print(QMainWindow.date, QMainWindow.t0, QMainWindow.task, QMainWindow.label, QMainWindow.n_sec - QMainWindow.countDown)
+
+            QMainWindow.countDown = QMainWindow.n_sec
+
         self.display.setText(displayTxt)
  
     def _createButtons(self): 
@@ -90,9 +108,11 @@ class PyPomodoro(QMainWindow):
         def press_start(): 
             # """Slot function."""
             if btn.text() == 'Start':
+                if QMainWindow.countDown == QMainWindow.n_sec:
+                    QMainWindow.t0 = QTime.currentTime().toString('hh:mm:ss') 
                 btn.setText("Pause") 
                 QMainWindow.pause = False
-                QMainWindow.t0 = QTime.currentTime()  
+                # QMainWindow.t0 = QTime.currentTime()  
             else:
                 btn.setText("Start")
                 QMainWindow.pause = True  
@@ -102,7 +122,17 @@ class PyPomodoro(QMainWindow):
             # """Slot function.""" 
             stp.setText("Stop") 
             btn.setText("Start") 
-            QMainWindow.countDown = 25*60 
+            print('log countdown') 
+            output_list= [QMainWindow.date, 
+                QMainWindow.t0, 
+                QMainWindow.task, 
+                QMainWindow.label, 
+                str(QMainWindow.n_sec - QMainWindow.countDown)]  
+            append_list_as_row('task_log.csv', output_list) 
+            print(QMainWindow.date, QMainWindow.t0, QMainWindow.task, QMainWindow.label, QMainWindow.n_sec - QMainWindow.countDown)
+
+
+            QMainWindow.countDown = QMainWindow.n_sec#25*60 
             QMainWindow.pause = True  
 
         # layout = QVBoxLayout()
@@ -119,7 +149,9 @@ class PyPomodoro(QMainWindow):
    
     def createTextbox(self):
         # self.textbox = QLineEdit(self)
-        self.generalLayout.addWidget(  QLineEdit('Task') )
+        tsk = QLineEdit('Task')
+        # btn.clicked.connect( press_start ) 
+        self.generalLayout.addWidget( tsk  )
         self.generalLayout.addWidget(  QLineEdit('Label') )
 
 
@@ -138,6 +170,9 @@ class PyPomodoro(QMainWindow):
 
         # keep window on top at all times
         self.setWindowFlags(Qt.WindowStaysOnTopHint)
+    
+        # first attempt at transparency 
+        self.setWindowOpacity(0.9)
 
         # set screen geometry
         screenGeom = QDesktopWidget().availableGeometry() 
@@ -162,10 +197,7 @@ class PyPomodoro(QMainWindow):
         self.createTextbox()
         self._createDisplay()
         self._createButtons()
-
-
-        # startup time
-        print( QTime.currentTime().toString('hh:mm:ss') )  
+ 
  
 
 
