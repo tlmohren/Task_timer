db1a8696b0f7607804c3d56e0fe5acc8b0499bae
tlmohren <tlmohren@uw.edu>
2020-05-02 17:02:43 -0700

    if from cmd, plotting function works

--- task_timer.py
+++ task_timer.py
@@ -83,7 +83,13 @@ class TaskTimer(QtWidgets.QMainWindow):
  
         self.pushButtonPlayPause.clicked.connect(self.onPlayPause)
         self.pushButtonStop.clicked.connect(self.onStop)
-        self.pushButtonPlot.clicked.connect(self.onPlot)
+
+        if topmost_folder == 'task_timer':
+
+
+            self.pushButtonPlot.clicked.connect(self.onPlot)
+
+
 
         # switch to prevent double plotting 
         self.bool_plot = False 
@@ -93,6 +99,7 @@ class TaskTimer(QtWidgets.QMainWindow):
         # self.pushButtonPlot.setIcon(QtGui.QIcon(  os.path.join( self.fig_dir, 'graph_icon.png' ) ))
         self.pushButtonPlot.setIcon(QtGui.QIcon(  os.path.join( self.fig_dir, 'graph_icon.ico' ) ))
         self.pushButtonPlot.setIconSize(QtCore.QSize(24,24))
+ 
  
     def lineEditTimerEvent(self): 
 
@@ -159,63 +166,75 @@ class TaskTimer(QtWidgets.QMainWindow):
 
 
     def onPlot(self):
+        import numpy as np
+        import glob
+        import pandas as pd
+        import matplotlib.pyplot as plt
+
+        # load project functions
+        package_path = 'D:\code_projects\\task_timer'
+        sys.path.append(package_path)
+        import analyze_log_functions as alf
+
         print('plotting')
-        # if self.bool_plot == False: 
-        #     # make colorscheme 
-        #     cols = np.array([
-        #         # [166,206,227], 
-        #     [31,120,180], 
-        #     [178,223,138], 
-        #     # [51,160,44], 
-        #     # [251,154,153], 
-        #     [227,26,28], 
-        #     # [253,191,111], 
-        #     [255,127,0], 
-        #     # [202,178,214], 
-        #     [106,61,154], 
-        #     [255,255,153]]) /255  
+        if self.bool_plot == False: 
+            # make colorscheme 
+            cols = np.array([
+                # [166,206,227], 
+            [31,120,180], 
+            [178,223,138], 
+            # [51,160,44], 
+            # [251,154,153], 
+            [227,26,28], 
+            # [253,191,111], 
+            [255,127,0], 
+            # [202,178,214], 
+            [106,61,154], 
+            [255,255,153]]) /255  
 
-        #     # load most recent log file   
-        #     log_files = sorted(glob.glob( self.log_dir+ '\*.csv')) 
-        #     df= pd.read_csv( log_files[-1], index_col=None, header=0)   
-              
-        #     # remove any task shorter than 1 minute, it's probably originated from a test
-        #     bool_short_duration = df['Duration (s)'] < 60
-        #     df = df[~bool_short_duration]
+            # load most recent log file   
+            log_files = sorted(glob.glob( self.log_dir+ '\*.csv')) 
+            df= pd.read_csv( log_files[-1], index_col=None, header=0)   
+            
+            # print()
+            # remove any task shorter than 1 minute, it's probably originated from a test
+            bool_short_duration = df['Duration (s)'] < 60
+            df = df[~bool_short_duration]
 
-        #     # modify data to useful format 
-        #     df['date'] = pd.to_datetime(df['date'] )  
-        #     df['Duration (hh:mm:ss)'] = pd.to_timedelta(df['Duration (s)'],'s') 
+            # modify data to useful format 
+            df['date'] = pd.to_datetime(df['date'] )  
+            df['Duration (hh:mm:ss)'] = pd.to_timedelta(df['Duration (s)'],'s') 
 
-        #     # find unique labels for color mapping
-        #     labels = df['Label'].unique()     
-        #     label_dict = {}
-        #     for label,col in zip(labels,cols): 
-        #         label_dict[label] = col  
+            # find unique labels for color mapping
+            labels = df['Label'].unique()     
+            label_dict = {}
+            for label,col in zip(labels,cols): 
+                label_dict[label] = col  
 
-        #     # plot figure 
-            # fig, ax = plt.subplots(2, 2, squeeze=False, figsize =(10,5))
+            # plot figure 
+            fig, ax = plt.subplots(2, 2, squeeze=False, figsize =(10,5))
              
-        #     # create 3 subplots
-        #     # alf.plot_week_tasks( ax[0,0], df, label_dict ) 
-        #     # alf.plot_time_spent_weekly(ax[0,1], df, label_dict  )
-        #     # alf.plot_time_spent_daily( ax[1,0], df, label_dict  ) 
+            # create 3 subplots
+            alf.plot_week_tasks( ax[0,0], df, label_dict ) 
+            alf.plot_time_spent_weekly(ax[0,1], df, label_dict  )
+            alf.plot_time_spent_daily( ax[1,0], df, label_dict  ) 
+            alf.plot_week_text(ax[1,1], df, ['Flex pendulum','FEA wing','Lab business', 'FF timing pend'], 'Time worked')
               
-        #     # adjust layout 
-        #     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=.4, hspace=None) 
-        #     ax[1,1].axes.remove() # remove unused lower right subplot 
+            # adjust layout 
+            plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=.4, hspace=None)  
+               
 
-        #     # show figure 
-        #     fig.canvas.draw()
-        #     fig.show()
+            # show figure 
+            fig.canvas.draw()
+            fig.show()
 
-        # #     # return plot properties to allow for closing later 
-        #     self.bool_plot = True 
-        #     self.fig = fig
-        #     return self
-        # else: 
-        #     plt.close(self.fig)
-        #     self.bool_plot = False
+        #     # return plot properties to allow for closing later 
+            self.bool_plot = True 
+            self.fig = fig
+            return self
+        else: 
+            plt.close(self.fig)
+            self.bool_plot = False
 
     def setColor(self):    
         p = self.palette() 
